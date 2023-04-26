06977c40e246d5e7fc6e46581b0582b65ab5a104
Grace Ku <gku@unc.edu>
2023-04-26 14:16:24 -0400

    fixed formatting

--- src/main/java/com/comp301/a09akari/controller/Controller.java
+++ src/main/java/com/comp301/a09akari/controller/Controller.java
@@ -3,55 +3,56 @@ package com.comp301.a09akari.controller;
 import com.comp301.a09akari.model.Model;
 
 public class Controller implements ClassicMvcController {
-    private Model model;
-    public Controller(Model model) {
-        if (model != null) {
-            this.model = model;
-        } else {
-            throw new IllegalArgumentException();
-        }
-    }
+  private final Model model;
 
-    @Override
-    public void clickNextPuzzle() {
-        if (model.getActivePuzzleIndex() + 1 < model.getPuzzleLibrarySize()) {
-            model.setActivePuzzleIndex(model.getActivePuzzleIndex() + 1);
-            model.resetPuzzle();
-        }
+  public Controller(Model model) {
+    if (model != null) {
+      this.model = model;
+    } else {
+      throw new IllegalArgumentException();
     }
+  }
 
-    @Override
-    public void clickPrevPuzzle() {
-        if (model.getActivePuzzleIndex() - 1 >= 0) {
-            model.setActivePuzzleIndex(model.getActivePuzzleIndex() - 1);
-            model.resetPuzzle();
-        }
+  @Override
+  public void clickNextPuzzle() {
+    if (model.getActivePuzzleIndex() + 1 < model.getPuzzleLibrarySize()) {
+      model.setActivePuzzleIndex(model.getActivePuzzleIndex() + 1);
+      model.resetPuzzle();
     }
+  }
 
-    @Override
-    public void clickRandPuzzle() {
-        int indexInt = 0;
-        double index = model.getPuzzleLibrarySize() * Math.random();
-        indexInt = (int) index;
-        while (indexInt == model.getActivePuzzleIndex()) {
-            index = model.getPuzzleLibrarySize() * Math.random();
-            indexInt = (int) index;
-        }
-        model.setActivePuzzleIndex(indexInt);
-        model.resetPuzzle();
+  @Override
+  public void clickPrevPuzzle() {
+    if (model.getActivePuzzleIndex() - 1 >= 0) {
+      model.setActivePuzzleIndex(model.getActivePuzzleIndex() - 1);
+      model.resetPuzzle();
     }
+  }
 
-    @Override
-    public void clickResetPuzzle() {
-        model.resetPuzzle();
+  @Override
+  public void clickRandPuzzle() {
+    int indexInt = 0;
+    double index = model.getPuzzleLibrarySize() * Math.random();
+    indexInt = (int) index;
+    while (indexInt == model.getActivePuzzleIndex()) {
+      index = model.getPuzzleLibrarySize() * Math.random();
+      indexInt = (int) index;
     }
+    model.setActivePuzzleIndex(indexInt);
+    model.resetPuzzle();
+  }
 
-    @Override
-    public void clickCell(int r, int c) {
-        if (model.isLamp(r, c)) {
-            model.removeLamp(r, c);
-        } else {
-            model.addLamp(r, c);
-        }
+  @Override
+  public void clickResetPuzzle() {
+    model.resetPuzzle();
+  }
+
+  @Override
+  public void clickCell(int r, int c) {
+    if (model.isLamp(r, c)) {
+      model.removeLamp(r, c);
+    } else {
+      model.addLamp(r, c);
     }
+  }
 }
--- src/main/java/com/comp301/a09akari/view/PuzzleView.java
+++ src/main/java/com/comp301/a09akari/view/PuzzleView.java
@@ -1,96 +1,92 @@
 package com.comp301.a09akari.view;
 
 import com.comp301.a09akari.controller.ClassicMvcController;
-import com.comp301.a09akari.controller.Controller;
 import com.comp301.a09akari.model.Model;
 import com.comp301.a09akari.model.ModelObserver;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.control.Label;
-import java.awt.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class PuzzleView implements FXComponent, ModelObserver {
-    private Model model;
-    private ClassicMvcController controller;
-    public PuzzleView(Model model, ClassicMvcController controller) {
-        this.model = model;
-        this.controller = controller;
-    }
+  private final Model model;
+  private final ClassicMvcController controller;
 
-    @Override
-    public Parent render() {
-        GridPane grid = new GridPane();
-        grid.getChildren().clear();
-        grid.setHgap(5);
-        grid.setVgap(5);
-        grid.setAlignment(Pos.CENTER);
+  public PuzzleView(Model model, ClassicMvcController controller) {
+    this.model = model;
+    this.controller = controller;
+  }
 
-        for (int r = 0; r < model.getActivePuzzle().getHeight(); r++) {
-            for (int c = 0; c < model.getActivePuzzle().getWidth(); c++) {
-                StackPane cell = new StackPane();
-                Rectangle pane = new Rectangle();
-                pane.setWidth(35);
-                pane.setHeight(35);
-                pane.setFill(Color.WHITE);
-                pane.setStroke(Color.BLACK);
-                cell.getChildren().add(pane);
-                switch (model.getActivePuzzle().getCellType(r, c)) {
-                    case CORRIDOR:
-                        int finalR = r;
-                        int finalC = c;
-                        cell.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                            @Override
-                            public void handle(MouseEvent mouseEvent) {
-                                controller.clickCell(finalR, finalC);
-                            }
-                        });
-                        if (model.isLamp(r, c)) {
-                            Image image = new Image("light-bulb.png");
-                            ImageView lamp = new ImageView();
-                            lamp.setImage(image);
-                            lamp.setFitWidth(25);
-                            lamp.setPreserveRatio(true);
-                            cell.getChildren().add(lamp);
-                        }
-                        if (model.isLit(r, c)) {
-                            pane.setFill(Color.YELLOW);
-                            if (model.isLamp(r, c) && model.isLampIllegal(r, c)) {
-                                pane.setFill(Color.RED);
-                            }
-                        }
-                        break;
-                    case CLUE:
-                        pane.setFill(Color.BLACK);
-                        Label clueNum = new Label("" + model.getActivePuzzle().getClue(r, c));
-                        clueNum.setTextFill(Color.WHITE);
-                        cell.getChildren().add(clueNum);
-                        if (model.isClueSatisfied(r, c)) {
-                            pane.setFill(Color.GREEN);
-                        }
-                        break;
-                    case WALL:
-                        pane.setFill(Color.BLACK);
-                        break;
-                }
-                grid.add(cell, c, r);
+  @Override
+  public Parent render() {
+    GridPane grid = new GridPane();
+    grid.getChildren().clear();
+    grid.setHgap(5);
+    grid.setVgap(5);
+    grid.setAlignment(Pos.CENTER);
+
+    for (int r = 0; r < model.getActivePuzzle().getHeight(); r++) {
+      for (int c = 0; c < model.getActivePuzzle().getWidth(); c++) {
+        StackPane cell = new StackPane();
+        Rectangle pane = new Rectangle();
+        pane.setWidth(35);
+        pane.setHeight(35);
+        pane.setFill(Color.WHITE);
+        pane.setStroke(Color.BLACK);
+        cell.getChildren().add(pane);
+        switch (model.getActivePuzzle().getCellType(r, c)) {
+          case CORRIDOR:
+            int finalR = r;
+            int finalC = c;
+            cell.setOnMouseClicked(
+                new EventHandler<MouseEvent>() {
+                  @Override
+                  public void handle(MouseEvent mouseEvent) {
+                    controller.clickCell(finalR, finalC);
+                  }
+                });
+            if (model.isLamp(r, c)) {
+              Image image = new Image("light-bulb.png");
+              ImageView lamp = new ImageView();
+              lamp.setImage(image);
+              lamp.setFitWidth(25);
+              lamp.setPreserveRatio(true);
+              cell.getChildren().add(lamp);
             }
+            if (model.isLit(r, c)) {
+              pane.setFill(Color.YELLOW);
+              if (model.isLamp(r, c) && model.isLampIllegal(r, c)) {
+                pane.setFill(Color.RED);
+              }
+            }
+            break;
+          case CLUE:
+            pane.setFill(Color.BLACK);
+            Label clueNum = new Label("" + model.getActivePuzzle().getClue(r, c));
+            clueNum.setTextFill(Color.WHITE);
+            cell.getChildren().add(clueNum);
+            if (model.isClueSatisfied(r, c)) {
+              pane.setFill(Color.GREEN);
+            }
+            break;
+          case WALL:
+            pane.setFill(Color.BLACK);
+            break;
         }
-
-        return grid;
+        grid.add(cell, c, r);
+      }
     }
 
-    @Override
-    public void update(Model model) {
-    }
+    return grid;
+  }
+
+  @Override
+  public void update(Model model) {}
 }
